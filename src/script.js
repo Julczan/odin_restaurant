3e93e8d6b3580d2cc467a6f8454c672c647b2434
Julczan <julek111222h@gmail.com>
2025-06-13 10:00:18 +0200

    moved to js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
 import "./styles.css";
+import cookImage from "./cook.jpg";
 
-console.log("test");
+const content = document.querySelector("#content");
+const image = document.createElement("img");
+const text = document.createElement("div");
+
+text.textContent = "Best Italian Food in Crakow";
+image.src = cookImage;
+
+content.appendChild(text);
+content.appendChild(image);
+
+console.log(content);
